914467edece8bffa565b98af3cb8c494617fd5fc
FarhanQuadri25 <zealfarhan@gmail.com>
2024-10-10 19:11:49 +0530

    new-commit

--- api/jobs.js
+++ api/jobs.js
@@ -1,5 +1,19 @@
-import jobsData from "./jobs.json";
+import { readFileSync } from "fs";
+import path from "path";
 
 export default function handler(req, res) {
-  res.status(200).json(jobsData);
+  try {
+    // Read the db.json file
+    const filePath = path.resolve(".", "jobs.json"); // Ensure the path is correct
+    const fileContents = readFileSync(filePath, "utf8");
+
+    // Parse JSON
+    const data = JSON.parse(fileContents);
+
+    // Send the jokes as the response
+    res.status(200).json(data.jobs);
+  } catch (error) {
+    console.error("Error reading jobs.json:", error);
+    res.status(500).json({ error: "Failed to read jokes" });
+  }
 }
--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -6,17 +6,20 @@ const JobListing = ({ isHome = false }) => {
   const [jobs, setJobs] = useState([]);
   const [loading, setLoading] = useState(true);
   const fetchJobs = async () => {
-    const apiUrl = isHome ? "api/jobs?_limit=3" : "api/jobs";
+    const apiUrl = isHome ? "/jobs.json?_limit=3" : "/jobs.json";
     try {
       const response = await fetch(apiUrl);
+      if (!response.ok) throw new Error("Network response was not ok");
       const data = await response.json();
-      setJobs(data);
+  
+      setJobs(data.jobs);
     } catch (error) {
       console.log("Error fetching data", error);
     } finally {
       setLoading(false);
     }
   };
+  
   useEffect(() => {
     fetchJobs();
   }, []);
