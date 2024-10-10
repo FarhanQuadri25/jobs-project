2a58f1b4a7e79a53c4a49c19b11fec6310598dd2
FarhanQuadri25 <zealfarhan@gmail.com>
2024-10-10 19:03:21 +0530

    new-commit

--- api/jobs.js
+++ api/jobs.js
@@ -1,19 +1,5 @@
-import { readFileSync } from "fs";
-import path from "path";
+import jobsData from "./jobs.json";
 
 export default function handler(req, res) {
-  try {
-    // Read the db.json file
-    const filePath = path.resolve(".", "jobs.json"); // Ensure the path is correct
-    const fileContents = readFileSync(filePath, "utf8");
-
-    // Parse JSON
-    const data = JSON.parse(fileContents);
-
-    // Send the jokes as the response
-    res.status(200).json(data.jobs);
-  } catch (error) {
-    console.error("Error reading jobs.json:", error);
-    res.status(500).json({ error: "Failed to read jokes" });
-  }
+  res.status(200).json(jobsData);
 }
